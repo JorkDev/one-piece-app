cc3a11081f0acc9c84f0baed430bf45110096a47
jorkdev <70247406+JorkDev@users.noreply.github.com>
2023-05-29 18:12:21 -0500

    Primer Commit

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import Head from 'next/head'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <h1>Let's build Once Piece App</h1>
+
+      {/* Header */}
+
+      {/* Nav */}
+
+      {/* Results */}
+      
+    </div>
+
+  )
+}
