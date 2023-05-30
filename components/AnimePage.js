8fbc4c2d972b392536eea9b98f4388704315c8da
jorkdev <70247406+JorkDev@users.noreply.github.com>
2023-05-30 11:25:20 -0500

    Actualizado display de elementos

--- components/AnimePage.js
+++ components/AnimePage.js
@@ -5,29 +5,29 @@ function AnimePage() {
   const [episodes, setEpisodes] = useState([]);
   const [page, setPage] = useState(1);
   const [hasMore, setHasMore] = useState(true);
-  const perPage = 20;  
+  const perPage = 10;  
 
   useEffect(() => {
     const getEpisodes = async () => {
       try {
         const response = await fetch(
-          `https://api.jikan.moe/v4/anime/21/episodes?page=${page}&limit=10`
+          `https://api.jikan.moe/v4/anime/21/episodes?page=${page}&limit=${perPage}`
         );
         const data = await response.json();
         if (Array.isArray(data.data)) {
-          setEpisodes((prevEpisodes) => [...prevEpisodes, ...data.data]);
+          setEpisodes(data.data);
         } else if (data.data) {
-          setEpisodes((prevEpisodes) => [...prevEpisodes, data.data]);
+          setEpisodes([data.data]);
         }
-        setPage((prevPage) => prevPage + 1);
+        setPage(1);
         setHasMore(data.data.length > 0);
       } catch (error) {
-        console.error("Error buscando episodios:", error);
+        console.error("Error fetching episodes:", error);
       }
-    };    
-
+    };
+  
     getEpisodes();
-  }, [page]);
+  }, []);
 
 
   return (
@@ -42,7 +42,7 @@ function AnimePage() {
           endMessage={<p>No more episodes to load.</p>}
         >
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
-            {episodes.map((episode) => (
+          {episodes.map((episode) => (
               <div
                 key={episode.mal_id}
                 className="bg-red-800 rounded-lg overflow-hidden hover:bg-red-700 hover:transform hover:scale-105 transition-all duration-300 cursor-pointer"
