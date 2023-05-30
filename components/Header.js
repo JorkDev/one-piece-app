27e0bc35d6c26c6df70bd739805384b0afa4bd4e
jorkdev <70247406+JorkDev@users.noreply.github.com>
2023-05-30 00:33:26 -0500

    Actualizados enlaces del header

--- components/Header.js
+++ components/Header.js
@@ -4,21 +4,34 @@ import {
   FilmIcon,
   TvIcon,
   UserIcon,
-  NewspaperIcon
+  NewspaperIcon,
 } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import HeaderItem from "./HeaderItem";
+import Link from "next/link";
 
 function Header() {
   return (
     <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
       <div className="flex flex-grow justify-evenly max-w-2xl">
-        <HeaderItem title="INICIO" Icon={HomeIcon} />
-        <HeaderItem title="MANGA" Icon={BookOpenIcon} />
-        <HeaderItem title="PELÍCULAS" Icon={FilmIcon} />
-        <HeaderItem title="TEMPORADAS" Icon={TvIcon} />
-        <HeaderItem title="PERSONAJES" Icon={UserIcon} />
-        <HeaderItem title="NOVEDADES" Icon={NewspaperIcon} />
+        <Link href="/">
+          <HeaderItem title={"INICIO"} Icon={HomeIcon} />
+        </Link>
+        <Link href="/manga">
+          <HeaderItem title="MANGA" Icon={BookOpenIcon} />
+        </Link>
+        <Link href="/movies">
+          <HeaderItem title="PELÍCULAS" Icon={FilmIcon} />
+        </Link>
+        <Link href="/seasons">
+          <HeaderItem title="TEMPORADAS" Icon={TvIcon} />
+        </Link>
+        <Link href="/characters">
+          <HeaderItem title="PERSONAJES" Icon={UserIcon} />
+        </Link>
+        <Link href="/news">
+          <HeaderItem title="NOVEDADES" Icon={NewspaperIcon} />
+        </Link>
       </div>
 
       <Image
