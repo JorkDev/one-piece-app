df329ba12cefdebd54244de4d221ecd107ad4f31
jorkdev <70247406+JorkDev@users.noreply.github.com>
2023-05-29 19:06:23 -0500

    Agregar logo al componente Header

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,15 @@
+import Image from "next/image"
+
 function Header() {
   return (
     <header className="">
         <h1>This is the header</h1>
+        <Image
+        src="/logo.png"
+        width={200}
+        height={200}
+        alt="Picture of the author"
+      />
     </header>
   )
 }
