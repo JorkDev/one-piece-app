b6904a1bd76f010b2985e8758aa18840c4b0b028
jorkdev <70247406+JorkDev@users.noreply.github.com>
2023-05-29 20:39:55 -0500

    Actualizado el height del header a auto

--- components/Header.js
+++ components/Header.js
@@ -11,7 +11,7 @@ import HeaderItem from "./HeaderItem";
 
 function Header() {
   return (
-    <header className="flex flex-col sm:flex-row m-5 justify-between items-center">
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
       <div className="flex flex-grow justify-evenly max-w-2xl">
         <HeaderItem title="INICIO" Icon={HomeIcon} />
         <HeaderItem title="MANGA" Icon={BookOpenIcon} />
