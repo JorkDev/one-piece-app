7a844fb8577613fb1ebdc8ca651b89933bd6b006
jorkdev <70247406+JorkDev@users.noreply.github.com>
2023-05-29 23:53:56 -0500

    Actualizado estilos del componente Nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,12 +1,15 @@
 import requests from "../utils/requests";
+import { useRouter } from "next/router";
 
 function Nav() {
   return (
     <nav>
-      <div>
+      <div className="flex px-10 sm:px-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
         {Object.values(requests).map(({ title, url }) => (
           <a key={title} href={url} target="_blank" rel="noopener noreferrer">
-            <h2>{title}</h2>
+            <h2 className="last:pr-5 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-blue-400">
+              {title}
+            </h2>
           </a>
         ))}
       </div>
