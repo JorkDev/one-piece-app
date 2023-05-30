f582ba8448ef3fdc99b7f247d17f3bf163329789
jorkdev <70247406+JorkDev@users.noreply.github.com>
2023-05-29 23:15:07 -0500

    Actualizados títulos de anime como links para el API

--- components/Nav.js
+++ components/Nav.js
@@ -4,8 +4,10 @@ function Nav() {
   return (
     <nav>
       <div>
-        {Object.values(requests).map(({ title }) => (
-          <h2 key={title}>{title}</h2>
+        {Object.values(requests).map(({ title, url }) => (
+          <a key={title} href={url} target="_blank" rel="noopener noreferrer">
+            <h2>{title}</h2>
+          </a>
         ))}
       </div>
     </nav>
